3d29a8e5ce574db21f425e5256b96c07fd90e269
Davi Miguel <davi.miguel2020@gmail.com>
2024-04-10 19:28:42 -0300

    Mapeamento concluído de um arquivo raw, com possibilidade de integrar webhook teams

--- mapeamento.js
+++ mapeamento.js
@@ -6,10 +6,24 @@ fetch(url)
   .then((res) => res.text())
   .then((dados) => {
     for (let texto of listaConteudos) {
-      console.log(texto);
+      // console.log(texto);
       if (dados.includes(texto)) {
         listaEncontrados.push(texto);
       }
     }
     return listaEncontrados;
+  })
+  .then((encontrados) => {
+    const naoAchados = listaConteudos.filter((conteudo, indice) => {
+      return !(encontrados[indice] == conteudo);
+    });
+    return naoAchados;
+  })
+  .then((naoAchados) => {
+    //Acionar o teams
+    if (!naoAchados.length != 0) {
+      console.log('LOG = Tudo ok com as screens que usamos.');
+    } else {
+      console.log(`Notificação no teams:\nAveriguar telas não encontradas nos bundles\nTelas não achadas=> ${naoAchados}`);
+    }
   });
